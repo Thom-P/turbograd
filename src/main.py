9a1889b3f24c4d03f7efa42decfe45d94770fd15
tplanes <tho.planes@gmail.com>
2024-01-10 00:45:44 +0100

    basic digit drawer ok

--- src/main.py
+++ src/main.py
@@ -1,20 +1,21 @@
 import tkinter as tk
 import tkinter.font as font
-from PIL import Image, ImageDraw
+from PIL import Image, ImageTk, ImageDraw
 
 
 class MyWindow:
     def __init__(self, root):
         root.title('Single Digit Recognition')
-        self.canvasSize = (400, 400)
-        self.canvas = tk.Canvas(root, 
-                                width=self.canvasSize[1], 
-                                height=self.canvasSize[0],
-                                background='black')
+        self.image = Image.new('L', (400, 400), color=0)
+        self.draw = ImageDraw.Draw(self.image)
+        self.tkImage = ImageTk.PhotoImage(self.image)
+        self.canvas = tk.Canvas(root, width=400, height=400)
+        self.imageContainer = self.canvas.create_image(0, 0, anchor="nw",
+                                                       image=self.tkImage)
         self.line = []
         self.canvas.bind("<Button-1>", self.initLine)
         self.canvas.bind("<B1-Motion>", self.drawLine)
-        # self.canvas.bind("<B1-ButtonRelease>", doneStroke)
+        # self.canvas.bind("<B1-ButtonRelease>", self.doneStroke)
 
         self.clearButton = tk.Button(root, text="Clear",
                                      command=self.clearCanvas)
@@ -29,27 +30,29 @@ class MyWindow:
         self.detectButton.pack()
 
     def initLine(self, event):
-        self.line = [(self.canvas.canvasx(event.x), self.canvas.canvasy(event.y))]
+        self.line = [(self.canvas.canvasx(event.x),
+                         self.canvas.canvasy(event.y))]
 
     def drawLine(self, event):
         x, y = self.canvas.canvasx(event.x), self.canvas.canvasy(event.y)
-        self.canvas.create_line((self.line[-1][0], self.line[-1][1], x, y),
-                                fill='white', width=10, capstyle='round')
         self.line.append((x, y))
+        self.draw.line(self.line, fill='white', width=15, joint='curve')
+        self.tkImage = ImageTk.PhotoImage(self.image)
+        self.canvas.itemconfig(self.imageContainer, image=self.tkImage)
+        # self.canvas.create_line((self.line[-1][0], self.line[-1][1], x, y),
+        #                         fill='white', width=10, capstyle='round')
 
     def clearCanvas(self):
-        self.canvas.delete('all')
+        self.image = Image.new('L', (400, 400), color=0)
+        self.draw = ImageDraw.Draw(self.image)
+        self.tkImage = ImageTk.PhotoImage(self.image)
+        self.canvas.itemconfig(self.imageContainer, image=self.tkImage)
 
     def detect(self):
-        # Cannot save canvas to image (workaround: draw with PIL) 
-        im = Image.new('L', self.canvasSize, color=0)
-        draw = ImageDraw.Draw(im)
-        draw.line(self.line, fill='white', width=10, joint='curve')
-        im.save('test.png')
-        
+        self.image.save('test.png')
 
-    # def doneStroke(event):
-    #    canvas.itemconfigure('currentline', width=1)
+    # def doneStroke(self, event):
+    #   pass
 
 
 root = tk.Tk()
