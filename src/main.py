0e2acb2b5acd0309e4204e74615ea8405c499997
tplanes <tho.planes@gmail.com>
2024-01-13 18:40:35 +0100

    added norm by bounding box, works better but still fails on 7/3, should try center by center of mass as done for dataset

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import tkinter.font as font
-from PIL import Image, ImageTk, ImageDraw
+from PIL import Image, ImageOps, ImageTk, ImageDraw
+import numpy as np
 
 ## tmp add detection here before packaging
 
@@ -29,6 +30,38 @@ class NeuralNetwork(nn.Module):
 
 model = torch.load('model.pth')
 model.eval()
+
+# Input preprocess followinmg MNIST procedure
+
+
+def preprocess_image(raw_image):
+    im_arr = np.array(raw_image)
+    is_row_empty = im_arr.any(1)
+    row_start = np.argmax(is_row_empty)
+    row_end = len(is_row_empty) - 1 - np.argmax(is_row_empty[::-1])
+
+    is_col_empty = im_arr.any(0)
+    col_start = np.argmax(is_col_empty)
+    col_end = len(is_col_empty) - 1 - np.argmax(is_col_empty[::-1])
+
+    # Normalize/center image
+    if row_end - row_start > col_end - col_start:
+        n_pix = row_end - row_start + 1
+        n_col = col_end - col_start + 1
+        arr_norm = np.zeros((n_pix, n_pix), dtype='uint8')
+        arr_norm[:, (n_pix - n_col) // 2:(n_pix - n_col) // 2 + n_col] \
+            = im_arr[row_start:row_end + 1, col_start:col_end + 1]
+    else:
+        n_pix = col_end - col_start + 1
+        n_row = row_end - row_start + 1
+        arr_norm = np.zeros((n_pix, n_pix), dtype = 'uint8')
+        arr_norm[(n_pix - n_row) // 2 : (n_pix - n_row) // 2 + n_row, :] \
+            = im_arr[row_start:row_end + 1, col_start:col_end + 1]
+
+    im_norm = Image.fromarray(arr_norm, mode='L')
+    im_20 = im_norm.resize((20, 20))
+    im_28 = ImageOps.expand(im_20, border=4, fill=0)
+    return im_28
 
 ## GUI
 
@@ -86,12 +119,13 @@ class MyWindow:
 
     def detect(self):
         #self.image.save('test.png')
-        im_28_28 = self.image.resize((28, 28))
-        X = transforms.ToTensor()(im_28_28) # this also does the 0 - 1 scaling
+        im_28_28 = preprocess_image(self.image)
+        #im_28_28 = self.image.resize((28, 28))
+        X = transforms.ToTensor()(im_28_28)  # this also does the 0 - 1 scaling
         y_pred = model(X)
-        #print(y_pred)
+        # print(y_pred)
         ind_max = y_pred.argmax()
-        #print(f'Detected a {ind_max}')
+        # print(f'Detected a {ind_max}')
         self.digit.set(f'Detected digit: {ind_max}')
     # def doneStroke(self, event):
     #   pass
