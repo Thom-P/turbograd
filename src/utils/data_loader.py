9261ad291f32e54ed8e4290fe1191ceb636edded
tplanes <tho.planes@gmail.com>
2024-01-22 17:03:15 +0100

    added auto detection trigger with done stroke

--- src/utils/data_loader.py
+++ src/utils/data_loader.py
@@ -2,19 +2,21 @@ import numpy as np
 
 
 def load_data(image_fname, label_fname, n_image):
+    """Load MNIST or EMNIST original format binary data: 28x28 images
+    and associated class labels.
+    """
     with open(image_fname, 'rb') as f:
         image_buf = f.read()
     with open(label_fname, 'rb') as f:
         label_buf = f.read()
 
-    # 4 int32 header (16 bytes): magic num=2051, n_image=60_000 or 10_000,
-    # n_row=28, n_col=28
+    # 4 int32 header (16 bytes): magic num=2051, n_image, n_row=28, n_col=28
     images = np.frombuffer(image_buf,
                            dtype='B',
                            count=28 * 28 * n_image,
                            offset=16).reshape(n_image, 28 * 28)
 
-    # 2 int32 header (8 bytes): magic number 2049, nb_items=60_000 or 10_000
+    # 2 int32 header (8 bytes): magic number 2049, n_item
     labels = np.frombuffer(label_buf,
                            dtype='B',
                            count=n_image,
--- src/utils/gui.py
+++ src/utils/gui.py
@@ -3,7 +3,8 @@ import tkinter.font as font
 from PIL import Image, ImageTk, ImageDraw
 
 
-# Tkinter GUI
+# Tkinter GUI for interactive drawing of char and converting into a
+# PIL Image for detection
 class MyWindow:
     def __init__(self, root, detect_fn):
         root.title('Single Character Detector')
@@ -14,30 +15,22 @@ class MyWindow:
         self.canvas = tk.Canvas(root, width=400, height=400)
         self.imageContainer = self.canvas.create_image(0, 0, anchor="nw",
                                                        image=self.tkImage)
-        self.msg = tk.StringVar()
-        self.msg.set('Detected char: ?')
-        self.label = tk.Label(root, textvariable=self.msg)
-        self.label['font'] = font.Font(size=30)
+        self.msg = tk.StringVar(value='Detected char: ?')
+        self.label = tk.Label(root, textvariable=self.msg, 
+                              font=font.Font(size=30))
 
         self.line = []
         self.canvas.bind("<Button-1>", self.initLine)
         self.canvas.bind("<B1-Motion>", self.drawLine)
-        # self.canvas.bind("<B1-ButtonRelease>", self.doneStroke)
+        self.canvas.bind("<B1-ButtonRelease>", self.doneStroke)
 
         self.clearButton = tk.Button(root, text="Clear",
-                                     command=self.clearCanvas)
-        self.clearButton['font'] = font.Font(size=30)
-
-        def detect_wrap():
-            return self.detect_fn(self.image, self.msg)
-        self.detectButton = tk.Button(root, text="Detect",
-                                      command=detect_wrap)
-        self.detectButton['font'] = font.Font(size=30)
+                                     command=self.clearCanvas,
+                                     font=font.Font(size=30))
 
         self.label.pack()
         self.canvas.pack()
-        self.clearButton.pack()  # side='left'
-        self.detectButton.pack()
+        self.clearButton.pack()
 
     def initLine(self, event):
         self.line = [(self.canvas.canvasx(event.x),
@@ -57,5 +50,5 @@ class MyWindow:
         self.canvas.itemconfig(self.imageContainer, image=self.tkImage)
         self.msg.set('Detected char: ?')
 
-    # def doneStroke(self, event):
-    #   pass
+    def doneStroke(self, event):
+        self.detect_fn(self.image, self.msg)
