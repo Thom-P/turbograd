654e8282918be8f5fd0894460f9eccb220c5e0b0
tplanes <tho.planes@gmail.com>
2024-01-22 20:10:13 +0100

    auto download and extract script

---
+++ download_dataset.sh
@@ -0,0 +1,13 @@
+data_dir='./EMNIST_dataset'
+curl https://www.itl.nist.gov/iaui/vip/cs_links/EMNIST/gzip.zip --output "$data_dir/gzip.zip" 
+FILES="emnist-balanced-train-images-idx3-ubyte.gz
+       emnist-balanced-test-images-idx3-ubyte.gz
+       emnist-balanced-train-labels-idx1-ubyte.gz
+       emnist-balanced-test-labels-idx1-ubyte.gz"
+
+for f in $FILES
+do
+ unzip -j "$data_dir/gzip.zip" "gzip/$f" -d $data_dir
+ gzip -d $data_dir'/'$f
+done
+
