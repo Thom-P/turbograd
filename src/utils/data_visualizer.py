1a3195955d1bdc55237749e3d7601079c557e474
tplanes <tho.planes@gmail.com>
2024-01-25 14:39:05 +0100

    typo in data_visualizer

--- src/utils/data_visualizer.py
+++ src/utils/data_visualizer.py
@@ -6,12 +6,12 @@ from PIL import Image
 from data_loader import load_data
 
 # Load train dataset
-n_test = 112_800  # number of images in train set
+n_train = 112_800  # number of images in train set
 n_class = 47  # number of classes
 train_images_f = '../../EMNIST_dataset/emnist-balanced-train-images-idx3-ubyte'
 train_labels_f = '../../EMNIST_dataset/emnist-balanced-train-labels-idx1-ubyte'
 
-train_images, train_labels = load_data(train_images_f, train_labels_f, n_test)
+train_images, train_labels = load_data(train_images_f, train_labels_f, n_train)
 
 # Choose 10 random occurences of each of the 47 classes and put the
 # corresponding 28 x 28 images in a 10 x 47 array
